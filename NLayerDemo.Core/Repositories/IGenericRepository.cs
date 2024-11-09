ef7416a7a9a04d83d19c8ff0a88392e19ea1d22d
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-09 15:30:17 +0300

    repositories tamamlandı

---
+++ NLayerDemo.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerDemo.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        T GetById(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void UpdateAsync(T entity);
+        void DeleteAsync(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+    }
+}
